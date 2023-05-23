009ab71c0318c9a85f95af091244d47c83e55e94
Eclipsorum <eclipsorum@gmail.com>
2023-05-24 00:44:23 +0700

    Simplify conditionals and other minor fixes

--- rock.js
+++ rock.js
@@ -1,15 +1,7 @@
 // Generate random number and assign it to a choice (rock, paper, or scissors)
 function getComputerChoice() {
     const randomNum = (Math.floor(Math.random() * 3));
-    if (randomNum === 0) {
-        return 'Rock';
-    }
-    else if (randomNum === 1) {
-        return 'Paper';
-    }
-    else {
-        return 'Scissors';
-    }
+    return randomNum === 0 ? 'Rock' : randomNum === 1 ? 'Paper' : 'Scissors';
 }
 
 // Start the game round by comparing choices and return the result (win, lose, or tie)
@@ -18,37 +10,19 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'Rock' || playerSelection === 'Paper' || playerSelection === 'Scissors') {
         switch(playerSelection) {
             case 'Rock':
-                if (computerSelection === 'Rock') {
-                    return "It's a tie! Rock vs Rock";                   
-                }
-                else if (computerSelection === 'Paper') {
-                    return "You Lose! Paper beats Rock";
-                }
-                else {
-                    return "You Win! Rock beats Scissors";
-                }
+                return computerSelection === 'Rock' ? "It's a tie!" :
+                computerSelection === 'Paper' ? "You Lose! Paper beats Rock" :
+                "You Win! Rock beats Scissors";
             
             case 'Paper':
-                if (computerSelection === 'Rock') {
-                    return "You Win! Paper beats Rock"; 
-                }
-                else if (computerSelection === 'Paper') {
-                    return "It's a tie! Paper vs Paper";
-                }
-                else {
-                    return "You Lose! Scissors beats Paper";
-                }
+                return computerSelection === 'Rock' ? "You Win! Paper beats Rock" :
+                computerSelection === 'Paper' ? "It's a tie!" :
+                "You Lose! Scissors beats Paper";
 
             case 'Scissors':
-                if (computerSelection === 'Rock') {
-                    return "You Lose! Rock beats Scissors"; 
-                }
-                else if (computerSelection === 'Paper') {
-                    return "You Win! Scissors beats Paper"; 
-                }
-                else {
-                    return "It's a tie! Scissors vs Scissors";
-                }
+                return computerSelection === 'Rock' ? "You Lose! Rock beats Scissors" :
+                computerSelection === 'Paper' ? "You Win! Scissors beats Paper" :
+                "It's a tie!";
         }
     }
 
@@ -60,37 +34,43 @@ function playRound(playerSelection, computerSelection) {
 
 // Start a 5-round game and compare the total score at the end to determine the winner
 function game() {
-    let round;
+    let round = 1;
     let playerScore = 0;
     let compScore = 0;
 
-    for (round = 1; round <= 5; round++) {
-        const playerSelection = prompt(`Round ${round}, Rock Paper Scissors?`);
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection, computerSelection);
-        console.log(`Round ${round}: ` + `You Chose ${playerSelection[0].toUpperCase() + 
-            playerSelection.slice(1).toLowerCase()}, ` + result);
-        if (result.includes('Win')) {
-            playerScore++;
+    while (round <= 5) {
+        let playerPrompt = prompt(`Round ${round}, Rock Paper Scissors?`);
+
+        if (playerPrompt) {
+            const playerSelection = playerPrompt;
+            const computerSelection = getComputerChoice();
+            const result = playRound(playerSelection, computerSelection);
+
+            console.log(`Round ${round}: ` + `${playerSelection[0].toUpperCase() + 
+                playerSelection.slice(1).toLowerCase()} VS ${computerSelection}, ` + result);
+    
+            if (result.includes('Win')) {
+                playerScore++;
+            }
+            else if (result.includes('Lose')) {
+                compScore++;
+            }
+            else if (result.includes('Bad')) {
+                round--;
+            }
         }
-        else if (result.includes('Lose')) {
-            compScore++;
+
+        else {
+            round = 5;
         }
-        else if (result.includes('Bad')) {
-            round--;
-        }
+        
+        round++;
     }
 
     console.log(`Your Score: ${playerScore}`);
     console.log(`Computer Score: ${compScore}`);
 
-    if (playerScore > compScore) {
-        console.log('Congrats! You Win');
-    }
-    else if (playerScore < compScore) {
-        console.log('Loser!');
-    }
-    else {
-        console.log('Tie');
-    }
+    playerScore > compScore ? console.log('Congrats! You Win') :
+    playerScore < compScore ? console.log('Loser!') : console.log('Tie');
+
 }
